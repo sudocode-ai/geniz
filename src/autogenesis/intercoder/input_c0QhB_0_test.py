06191143556965ab3f400ecfc7789b12fddbafd4
Ning Ren <renning22@gmail.com>
2024-05-26 19:16:49 -0700

    ckpt 1

---
+++ src/autogenesis/intercoder/input_c0QhB_0_test.py
@@ -0,0 +1,26 @@
+import sudocode
+from input import make_palindrome
+
+
+@sudocode.DebugAgent()
+def test_make_palindrome():
+    assert make_palindrome('') == ''
+    assert make_palindrome('cat') == 'catac'
+    assert make_palindrome('contest') == 'tcoentoc'
+    assert make_palindrome('ai') == 'iai'
+    assert make_palindrome('123') == '321eedi'
+    assert make_palindrome('Hello') == 'HellolleH'
+    assert make_palindrome('World') == 'dRowodlrW'
+    assert make_palindrome('short') == 'thsuorahttslhorst'
+    assert make_palindrome('extraordinarily_long') == 'ynatteroynegdne_lartucroserontoanelorarye'
+    assert make_palindrome('A!') == 'A!'
+    assert make_palindrome('1#!') == '1#!1#'
+    assert make_palindrome('hello world!') == '!ollehdlrowolleh'
+    assert make_palindrome('a') == 'a'
+    assert make_palindrome('aba') == 'ababa'
+    import random
+    import string
+    long_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=50))
+    palindrome_length = len(long_str) // 4
+    assert make_palindrome(long_str) == long_str[:palindrome_length] + long_str[::-1]
+test_make_palindrome()
