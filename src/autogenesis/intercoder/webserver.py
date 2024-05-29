d47ec775bd6cfecc3abb3b99aa30fa99e4ddf159
Ning Ren <renning22@gmail.com>
2024-05-29 02:35:03 -0700

    ckpt 7

--- src/autogenesis/intercoder/webserver.py
+++ src/autogenesis/intercoder/webserver.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -33,15 +34,20 @@ def get_prompt():
     return code_file
 
 
-# def click_run_all_tests():
-#     play_all_tests()
-#     test_dist = get_test_dist()
-#     get_test_info()
-
-
 test_info = get_test_info()
 
 with gr.Blocks() as demo:
+    test_info_state = gr.State(test_info)
+
+    def click_run_all_tests():
+        print('click_run_all_tests')
+        test_info = get_test_info()
+        import random
+        random.shuffle(test_info)
+        return {
+            test_info_state: test_info,
+        }
+
     candidate_boxes = []
     test_boxes = []
     with gr.Row():
@@ -58,7 +64,7 @@ with gr.Blocks() as demo:
     with gr.Row(equal_height=True):
         with gr.Column():
             file_explorer = gr.FileExplorer(
-                every=2,
+                # every=2,
                 glob='**/*.py',
                 file_count='single',
                 height=200)
@@ -71,27 +77,37 @@ with gr.Blocks() as demo:
                 elem_id="code_editor",
                 scale=3)
         with gr.Column():
-            for info in test_info:
-                with gr.Group():
-                    with gr.Row():
-                        test_box = gr.Textbox(
-                            info['call_str'], show_label=False, interactive=True)
-                        test_boxes.append(test_box)
-                    with gr.Row():
-                        output_options = info['outputs']
-                        output_radio_group = gr.Radio(
-                            choices=output_options,
-                            value=output_options[0],
-                            container=False,
-                            interactive=True,
-                            label='Output')
-                    lock_checkbox = gr.Checkbox(label='Lock')
+            @gr.render(inputs=[test_info_state], triggers=[run_all_tests_button.click, gen_code_button.click, gen_test_button.click])
+            def render_test_data(input_0):
+                # with open('test_dist.json', 'r') as f:
+                #     input_0 = json.load(f)
+                if input_0 is None:
+                    print('input_0 is None')
+                    return
+                for info in input_0:
+                    with gr.Group():
+                        with gr.Row():
+                            test_box = gr.Textbox(
+                                info['call_str'], show_label=False, interactive=True)
+                            test_boxes.append(test_box)
+                        with gr.Row():
+                            output_options = info['outputs']
+                            output_radio_group = gr.Radio(
+                                choices=output_options,
+                                value=output_options[0],
+                                container=False,
+                                interactive=True,
+                                label='Output')
+                        lock_checkbox = gr.Checkbox(label='Lock')
 
     gen_code_button.click(gen_code)
     gen_test_button.click(gen_test)
-    # run_all_tests_button.click()
+    run_all_tests_button.click(
+        click_run_all_tests, inputs=[], outputs=[test_info_state])
     file_explorer.change(select_candidate_file, file_explorer, code_editor)
     # code_editor.change(code_editor_change, code_editor, None)
+    # gr.on(triggers=None, fn=click_run_all_tests, inputs=[], every=2)
+    # dep = demo.load(click_run_all_tests, inputs=[], outputs=[test_info_state], every=2)
 
 if __name__ == "__main__":
     ray.init(include_dashboard=False, ignore_reinit_error=True)
